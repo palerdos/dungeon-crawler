adb05272a68a17cafdac911fff3caa6497d72f50
palerdos <palerdos95@gmail.com>
2021-03-10 23:39:27 +0100

    feat: weapon and shield names are generated randomly from a collection of names

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Shield.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Shield.java
@@ -4,10 +4,21 @@ import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.util.RandomHelper;
 
 public class Shield extends Item {
+    public static final String[] SHIELD_NAMES = {
+            "Kingdom's Pride",
+            "Hero's Calling",
+            "Honor's Grasp",
+            "Lich Shield Wall",
+            "Howling Armament",
+            "Crazed Oak Barricade",
+            "Vanquisher Hardwood Armament",
+            "Chaos, Vengeance of Blessings",
+            "Kingdom's Honor, Destroyer of Stealth",
+            "Amnesia, Reach of Grieving Widows"
+    };
 
     public Shield(Cell cell) {
-        super(cell, "Shield", RandomHelper.getRandomNumber(1, 3));
-        // TODO random name generator
+        super(cell, RandomHelper.getRandomName(SHIELD_NAMES), RandomHelper.getRandomNumber(1, 3));
     }
 
     @Override
--- src/main/java/com/codecool/dungeoncrawl/logic/items/Weapon.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Weapon.java
@@ -4,10 +4,22 @@ import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.util.RandomHelper;
 
 public class Weapon extends Item{
+    public static final String[] WEAPON_NAMES = {
+            "Etherius",
+            "Silence",
+            "Widow Maker",
+            "Fire Infused Carver",
+            "Eternal Mageblade",
+            "Vengeful Glass Swiftblade",
+            "Dire Mithril Sabre",
+            "Nightcrackle, Promise of the Void",
+            "Scar, Skewer of Mourning",
+            "Doombringer, Longblade of the Lion"
+    };
+
 
     public Weapon(Cell cell) {
-        super(cell, "Weapon", RandomHelper.getRandomNumber(2, 5));
-        // TODO random name generator
+        super(cell, RandomHelper.getRandomName(WEAPON_NAMES), RandomHelper.getRandomNumber(2, 5));
     }
 
     @Override
